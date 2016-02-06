3a3b09a1356d7b3bd6acd68b96975034da16e8ce
John Morrice <john@functorama.com>
2016-02-06 00:33:10 +0000

    Use Hackers Delight Algorithm

--- src/github.com/johnny-morrice/codility/one/solution/one.go
+++ src/github.com/johnny-morrice/codility/one/solution/one.go
@@ -1,29 +1,48 @@
 package solution
 
 func Solution(N int) int {
+    // Derive int length
     const maxu = ^uint(0)
     const maxu64 = ^uint64(0)
     bits := uint(32)
     if uint64(maxu) == maxu64 {
         bits = 64
     }
-    longest := 0
-    count := 0
-    lefthit := false
+
+    // Count leading/trailing bits
+    lead := uint(0)
+    trail := uint(0)
+    lcnt, tcnt := true, true
     for i := uint(0); i < bits; i++ {
-        b := int(1) << i
-        if N & b != 0 {
-            if lefthit {
-                if count > longest {
-                    longest = count
-                    count = 0
-                }
-            } else {
-                lefthit = true
-            }
+        l := 1 << i
+        t := 1 << (bits - 1 - i)
+        if N & l == 0 && lcnt {
+            lead++
         } else {
-            count++
+            lcnt = false
+        }
+        if N & t == 0 && tcnt {
+            trail++
+        } else {
+            tcnt = false
         }
     }
-    return longest
+
+    // Remove lead
+    N = N >> lead
+    max := trail + lead
+    // Set trailing high
+    for i := uint(0); i < max; i++ {
+        b := 1 << (bits - 1 - i)
+        N = N | b
+    }
+
+    // Hacker's delight
+    N = ^N
+    k := 0
+    for ; N > 0; k++ {
+        N = N & (2*N)
+    }
+
+    return k
 }
