5c253500158996c260eb63f1f48430eb550506c8
John Morrice <john@functorama.com>
2016-02-10 11:18:42 +0000

    Use for range loop

--- src/github.com/johnny-morrice/codility/two/solution/two.go
+++ src/github.com/johnny-morrice/codility/two/solution/two.go
@@ -3,10 +3,10 @@ package solution
 func Solution(A []int, K int) []int {
     cnt := len(A)
     shift := make([]int, cnt)
-    for i := 0; i < cnt; i++ {
+    for i, a := range A {
         j := i + K
         j = j % cnt
-        shift[j] = A[i]
+        shift[j] = a
     }
     return shift
 }
